1407196eb1aaa5aa008e98af16cddcc79e3a69c5
Vaibhavr699 <78257023+Vaibhavr699@users.noreply.github.com>
2025-06-18 14:37:02 +0530

    Frontend

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -75,4 +78,5 @@ export default defineConfig({
       }
     }
   }
-})
+});
+
